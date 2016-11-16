64327b4cd123aa38593996e61d95ac669875b3e2
katerina1987 <caroline1987hlj@163.com>
2016-11-16 15:07:07 +0800

    douban_json

--- firstPython.py
+++ firstPython.py
@@ -8,13 +8,53 @@
 
 #print arg()
 
+# import json
+# import requests
+# import urllib
+#
+# urls = ["https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=time&page_limit=20&page_start={}".format(str(i)) for i in range(0,300,20)]
+#
+# for single_url in urls:
+#     urlData =urllib.request.urlopen(single_url).read()
+#     urlData = urlData.decode("utf-8")
+#     s = json.loads(urlData)
+#     for i in range(0,10):
+#         urlData1 = s["subjects"][i]["url"]
+#         print(urlData1)
 
-url = "https:\/\/movie.douban.com\/subject\/26411201\/"
+import requests
+import lxml
+from bs4 import BeautifulSoup
 
-afterUrl = url.split("\/",5)
-urlSplit = ""
+url = "https://movie.douban.com/subject/25895257/"
 
-for i in range(0,5):
-    urlSplit += afterUrl[i]+"/"
+wbData = requests.get(url)
 
-print(urlSplit)
+soup = BeautifulSoup(wbData.text, "lxml")
+
+titles = soup.select("#content > h1 > span")
+years = soup.select("#content > h1 > span.year")
+directors = soup.select("#info > span > span.attrs > a")
+scriptwriters = soup.select("#info > span > span.attrs > a")
+actors = soup.select("#info > span.actor > span.attrs > a")
+types = soup.select("#info > span")
+prices = soup.select("#interest_sectl > div.rating_wrap.clearbox > div.rating_self.clearfix > strong")
+evaluationNumbers = soup.select(
+    "#interest_sectl > div.rating_wrap.clearbox > div.rating_self.clearfix > div > div.rating_sum > a > span")
+print(types)
+
+# for title, year, director, scriptwriter, price, evaluationNumber in zip(titles, years, directors, scriptwriters, prices,
+#                                                                         evaluationNumbers):
+#     data = {
+#         "title": title.get_text(),
+#         "year": year.get_text(),
+#         "director": director.get_text(),
+#         "scriptwriter": list(scriptwriter[0].stripped_strings),
+#         "price": price.get_text(),
+#         "evaluationNumber": evaluationNumber.get_text()
+#     }
+#     print(data)
+
+
+
+
--- text_douban_load.py
+++ text_douban_load.py
@@ -1,36 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import json
+import urllib
 
-urls = ["https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start={}".format(str(i)) for i in range(20,200,20)]
+urls = ["https://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=time&page_limit=20&page_start={}".format(str(i)) for i in range(0,300,20)]
 
 for single_url in urls:
-    # print(single_url)
-
-# def ger_url_link(url,dataUrl = None):
-    '''
-    urlData=requests.get(single_url)
-
-    soups = BeautifulSoup(urlData.text,"lxml")
-    dataUrl = []
-    for urlLink in soups:
-        dataUrl = {
-             "urlLink":soups.select("url")
-        }
-        print(dataUrl)
-    '''
-
-dataUrl = ["https:\/\/movie.douban.com\/subject\/26628357\/","https:\/\/movie.douban.com\/subject\/3868141\/","https:\/\/movie.douban.com\/subject\/26592040\/"]
-dataUrlSplits = []
-for i in dataUrl:
-
-    afterUrl = i.split("\/", 5)
-    urlSplit = ""
-    for i in range(0, 5):
-        urlSplit += afterUrl[i] + "/"
-    dataUrlSplits.append(urlSplit)
-print(dataUrlSplits)
-
+    urlData =urllib.request.urlopen(single_url).read()
+    urlData = urlData.decode("utf-8")
+    s = json.loads(urlData)
+    for i in range(0,10):
+        urlData1 = s["subjects"][i]["url"]
+print(urlData1)
 
 def get_data(url,data = None):
     wbData = requests.get(url)
@@ -39,21 +21,30 @@ def get_data(url,data = None):
 
     titles = soup.select("#content > h1 > span")
     years = soup.select("#content > h1 > span.year")
+    directors = soup.select("#info > span > span.attrs > a")
+    scriptwriters = soup.select("#info > span > span.attrs > a")
     prices = soup.select("#interest_sectl > div.rating_wrap.clearbox > div.rating_self.clearfix > strong")
     evaluationNumbers = soup.select("#interest_sectl > div.rating_wrap.clearbox > div.rating_self.clearfix > div > div.rating_sum > a > span")
     # print(titles,years,prices,evaluationNumbers)
 
-    for title, year, price, evaluationNumber in zip(titles,years,prices,evaluationNumbers):
+    for title, year, director,scriptwriter, price, evaluationNumber in zip(titles,years,directors,scriptwriters,prices,evaluationNumbers):
         data = {
             "title": title.get_text(),
             "year" : year.get_text(),
+            "director":director.get_text(),
+            "scriptwriter":list(scriptwriter[0].stripped_strings),
             "price": price.get_text(),
             "evaluationNumber": evaluationNumber.get_text()
         }
         print(data)
 
-for  i in dataUrlSplits:
-    get_data(i)
+# for  i in urlData1:
+get_data(urlData1)
+
+
+
+
+
 
 
 
